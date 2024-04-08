e532002795c0945e05acd6edadb32b26175ea695
Abat <personal@abat.me>
2024-04-08 08:45:08 +0200

    Add error boundary

--- src/telegram.js
+++ src/telegram.js
@@ -3,4 +3,9 @@ const grammy = require('grammy')
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN
 const bot = new grammy.Bot(TOKEN)
 
+bot.catch((err) => {
+  err.ctx.reply('An error occurred. Please try again later.')
+  console.log('An error occurred:', err)
+})
+
 module.exports = bot

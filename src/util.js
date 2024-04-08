fcbc4c6ed70956cdb0324fbb8316e3e9be326ece
Abat <personal@abat.me>
2024-04-08 09:00:08 +0200

    clear rule for html formatting

--- src/util.js
+++ src/util.js
@@ -1,1 +1,13 @@
-exports.weatherChatSystemMessage = `You are a weather bot. Use only one measurement system depending on the country but no need to include country name on the reponse. Provide clothing recommendations based on the user's raw weather data. RESPONSE MUST BE VALID HTML formatted (ONLY FOR HIGHLIGHTING PURPOSES NO META DATA SHOULD BE ATTACHED).`
+const ALLOWED_HTML_FORMAT = `<b>bold</b>, <strong>bold</strong>
+<i>italic</i>, <em>italic</em>
+<u>underline</u>, <ins>underline</ins>
+<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+<span class="tg-spoiler">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>
+<b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
+<a href="http://www.example.com/">inline URL</a>
+<code>inline fixed-width code</code>
+<pre>pre-formatted fixed-width code block</pre>
+<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
+<blockquote>Block quotation started\nBlock quotation continued\nThe last line of the block quotation</blockquote>`
+
+exports.weatherChatSystemMessage = `You are a weather bot. Use only one measurement system depending on the country but no need to include country name on the reponse. Provide clothing recommendations based on the user's raw weather data. RESPONSE MUST BE VALID HTML FORMATTED ON THE SCOPE OF THESE TAGS ALLOWED HTML FORMAT NO OTHER TAGS CANNOT BE INCLUDED: ${ALLOWED_HTML_FORMAT}.`

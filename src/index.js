843842504bb280730803860ad1a424fb790dd83a
Abat <personal@abat.me>
2024-04-08 08:54:41 +0200

    Restructure

--- src/index.js
+++ src/index.js
@@ -5,21 +5,10 @@ const { webhookCallback } = require('grammy')
 const app = express()
 app.use(express.json())
 
-const openAI = require('./openAI')
 const telegramBot = require('./telegram')
-const { getWeather, getWeatherByCoords } = require('./weather')
 const { GET, POST } = require('./wa')
 const { privacyPolicy, termsOfService } = require('./legal')
-const { weatherChatSystemMessage } = require('./util')
 const { deleteDataRequest, getDeletionStatus } = require('./deletion')
-
-const query = process.env.GEO_QUERY
-
-MODELS = {
-  GPT3: 'gpt-3.5-turbo',
-  GPT4: 'gpt-4',
-  GPT4_TURBO: 'gpt-4-turbo-preview',
-}
 
 app.get('/privacy-policy', privacyPolicy)
 app.get('/terms-of-service', termsOfService)
@@ -29,60 +18,6 @@ app.post('/meta/delete', deleteDataRequest)
 app.get('/deletion', getDeletionStatus)
 
 app.post('/telegram/webhook', webhookCallback(telegramBot, 'express'))
-
-async function main() {
-  const geoData = await getWeather(query)
-  const lat = geoData.lat
-  const lon = geoData.lon
-  const weatherDataStr = await getWeatherByCoords(lat, lon)
-
-  const chatCompletion = await openAI.chat.completions.create({
-    messages: [
-      { role: 'system', content: weatherChatSystemMessage },
-      { role: 'user', content: weatherDataStr },
-    ],
-    model: MODELS.GPT4_TURBO,
-  })
-
-  console.log(chatCompletion.choices[0].message.content)
-}
-
-telegramBot.command('start', async (ctx) => {
-  await ctx.reply(
-    'Hello buddy!\n/weather to get help with weather.\n/wear to get help with what to wear.'
-  )
-})
-
-telegramBot.command('weather', async (ctx) => {
-  const geoData = await getWeather(query)
-  const lat = geoData.lat
-  const lon = geoData.lon
-  const weatherDataStr = await getWeatherByCoords(lat, lon)
-
-  await ctx.reply('```json\n' + weatherDataStr + '\n```', {
-    parse_mode: 'MarkdownV2',
-  })
-})
-
-telegramBot.command('wear', async (ctx) => {
-  const geoData = await getWeather(query)
-  const lat = geoData.lat
-  const lon = geoData.lon
-  await ctx.replyWithChatAction('typing')
-  const weatherDataStr = await getWeatherByCoords(lat, lon)
-
-  const chatCompletion = await openAI.chat.completions.create({
-    messages: [
-      { role: 'system', content: weatherChatSystemMessage },
-      { role: 'user', content: weatherDataStr },
-    ],
-    model: MODELS.GPT4_TURBO,
-  })
-
-  await ctx.reply(chatCompletion.choices[0].message.content, {
-    parse_mode: 'MarkdownV2',
-  })
-})
 
 const port = process.env.PORT ?? 3000
 
--- src/telegram.js
+++ src/telegram.js
@@ -1,7 +1,55 @@
 const grammy = require('grammy')
 
+const openAI = require('./openAI')
+const { weatherChatSystemMessage } = require('./util')
+const { getWeather, getWeatherByCoords } = require('./weather')
+
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN
+const MODELS = {
+  GPT3: 'gpt-3.5-turbo',
+  GPT4: 'gpt-4',
+  GPT4_TURBO: 'gpt-4-turbo-preview',
+}
+
 const bot = new grammy.Bot(TOKEN)
+
+bot.command('start', async (ctx) => {
+  await ctx.reply(
+    'Hello buddy!\n/weather to get help with weather.\n/wear to get help with what to wear.'
+  )
+})
+
+bot.command('weather', async (ctx) => {
+  const geoData = await getWeather(process.env.GEO_QUERY)
+  const lat = geoData.lat
+  const lon = geoData.lon
+  const weatherDataStr = await getWeatherByCoords(lat, lon)
+  const niceFormatted = JSON.stringify(JSON.parse(weatherDataStr), null, 2)
+
+  await ctx.reply('```json\n' + niceFormatted + '\n```', {
+    parse_mode: 'MarkdownV2',
+  })
+})
+
+bot.command('wear', async (ctx) => {
+  const geoData = await getWeather(process.env.GEO_QUERY)
+  const lat = geoData.lat
+  const lon = geoData.lon
+  await ctx.replyWithChatAction('typing')
+  const weatherDataStr = await getWeatherByCoords(lat, lon)
+
+  const chatCompletion = await openAI.chat.completions.create({
+    messages: [
+      { role: 'system', content: weatherChatSystemMessage },
+      { role: 'user', content: weatherDataStr },
+    ],
+    model: MODELS.GPT4_TURBO,
+  })
+
+  await ctx.reply(chatCompletion.choices[0].message.content, {
+    parse_mode: 'MarkdownV2',
+  })
+})
 
 bot.catch((err) => {
   err.ctx.reply('An error occurred. Please try again later.')
